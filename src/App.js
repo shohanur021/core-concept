57da4823c454471f32fd44037edc7adb903c3a72
shohanur021@gmail.com <shohanur021@gmail.com>
2021-03-26 13:16:22 +0600

    2nd time

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var headerStyle = {
+    backgroundColor:"red",
+    color:"white"
+  }
+  var student = {
+    name:"Shohan",
+    age:20
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 className="heading" style={headerStyle}>My first paragraph.</h3>
+        <p style={{border:"2px solid yellow",color:"white"}}>I am sajib</p>
+        <p>I am {student.name}.My age is {student.age} not {12+13}</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  var style = {
+    border:"2px solid red",
+    margin:"5px",
+    padding:"0px 10px"
+  }
+  return(
+    <div style={style}>
+      <h2>I am a person</h2>
+      <p>Hey listen to me.</p>
+    </div>
+  )
 }
 
 export default App;
